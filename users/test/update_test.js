afd1e7418dfea441ca168a94fefb76e10fe80b5c
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-12-02 13:26:48 -0200

    Using update operations with mongoose

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -6,17 +6,78 @@ describe('updating users', () => {
     let joe = {};
 
     beforeEach(done => {
-        joe = new User({ name });
+        joe = new User({
+            name,
+            postCount : 0
+        });
         joe.save().then(user => done());
     });
 
+    function assertName(operation, newName) {
+        return operation
+            .then(user => User.findOne({ name : newName }))
+            .then(user => {
+                assert(user !== null);
+                return user;
+            })
+            .then(user => assert(newName === user.name))
+    }
+
     it('instance type using set and save', done => {
         const newName = 'Alex';
-
         joe.set('name', newName);
-        joe.save()
-            .then(user => User.findOne({ name : newName }))
-            .then(user => assert(newName === user.name))
+
+        assertName(joe.save(), newName)
             .then(() => done());
+    });
+
+    it('a model instance can update', done => {
+        const newName = 'Alex';
+
+        assertName(joe.update({ name : newName }), newName)
+            .then(() => done());
+    });
+
+    it('a model class can update', done => {
+        const newName = 'Alex';
+
+        assertName(
+            User.update({ name : joe.name }, { name : newName }),
+            newName
+        )
+        .then(() => done());
+    });
+
+    it('a model class can update one record', done => {
+        const newName = 'Alex';
+
+        assertName(
+            User.findOneAndUpdate({ name : joe.name }, { name : newName }),
+            newName
+        )
+        .then(() => done());
+    });
+
+    it('a model class can find a record with an id and update', done => {
+        const newName = 'Alex';
+        
+        assertName(
+            User.findByIdAndUpdate(joe._id, { name : newName }),
+            newName
+        )
+        .then(() => done());
+    });
+
+    it('a user can have their postCount incremented by 1', done => {
+        User.update({ 
+            name 
+        }, { 
+            $inc : {
+                postCount: 1 
+            } 
+        })
+        .then(user => User.findOne({ name }))
+        .then(user => assert(user.postCount === 1))
+        .then(() => done());
     });
 });
