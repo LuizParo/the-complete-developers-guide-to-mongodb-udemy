4373cf7dc8b8dd304bd4a4acd76db8ae005f3fe1
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-12-17 21:35:09 -0200

    creating the routes to manage the drivers

---
+++ muber/test/app.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../app');
+
+describe('the express app', () => {
+
+    it('handles a GET request to /api', done => {
+        request(app)
+            .get('/api')
+            .end((err, resp) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+
+                assert(resp.body.hi === 'there');
+                done();
+            });
+        });
+});
